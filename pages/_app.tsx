525c7644af96218ae61ab9950b1c0d5e301e3ffe
LaChancee <miharisoababef@gmail.com>
2023-03-14 22:26:28 +0100

    Imports inutiles retirés

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import getStripe from '../utils/get-stripe'
+import {Elements} from "@stripe/react-stripe-js";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <Elements stripe={getStripe()}>
+        <Component {...pageProps} />
+      </Elements>
+  )
 }
