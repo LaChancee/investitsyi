7bfe2b1cefcc518bf89ba0d5a5688c96141a0774
LaChancee <miharisoababef@gmail.com>
2023-03-18 01:21:15 +0100

    Transfert des fichers de App.tsx => index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import cardDeco from 'public/cards-deco.png'
 import clientGars from 'public/customers/client1.jpg'
 import { GoogleFonts } from 'next-google-fonts'
 import Layout from "./Layout";
+import Link from "next/link";
 const App: React.FC = () => {
     return (
         <Layout>
@@ -25,7 +26,7 @@ const App: React.FC = () => {
                             </h1>
                             <p className="max-w-2xl mb-6  text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Découvrez les tendances du marché, apprenez les astuces des investisseurs les plus prospères et investissez en toute confiance avec notre plateforme.
                             </p>
-                            <a href="#"
+                            <Link href="/AboutUs"
                                className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-full bg-primary transition duration-300 hover:shadow-xl hover:shadow-primary/30  focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                                 Nous découvrir dès maintenant
                                 <svg className="w-5 h-5 ml-2 -mr-1 " fill="currentColor" viewBox="0 0 20 20"
@@ -34,10 +35,10 @@ const App: React.FC = () => {
                                           d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                                           clip-rule="evenodd"></path>
                                 </svg>
-                            </a>
+                            </Link>
                             <a href="#"
                                className="inline-flex items-center transition justify-center px-5 py-3 text-base font-medium text-center text-primary underline underline-offset-4  rounded-lg hover:decoration-2 focus:ring-4 focus:ring-gray-100 ">
-                                Comment ça marche
+                                Comment ça marche ?
                             </a>
                         </div>
                         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
