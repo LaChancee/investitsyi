4f4a6743e0d59e15cd644cef574e6b83d3424944
LaChancee <miharisoababef@gmail.com>
2023-03-29 21:49:14 +0200

    Ajout des session providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css';
 import Layout from './Layout';
+import {SessionProvider} from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-            <Component {...pageProps} />
+        <SessionProvider session={pageProps.session}>
+                <Component {...pageProps} />
+        </SessionProvider>
     );
 }
 
