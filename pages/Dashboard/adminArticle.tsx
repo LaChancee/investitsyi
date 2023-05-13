e53798ee14541c2e704a417450c7a425229f22a9
LaChancee <miharisoababef@gmail.com>
2023-05-13 15:33:14 +0200

    Intégrationn du formulaire pour poster un article

---
+++ pages/Dashboard/adminArticle.tsx
@@ -0,0 +1,143 @@
+import DashboardLayout from "./DashboardLayout";
+import React, {ChangeEvent, FormEvent, useState} from "react";
+import 'react-quill/dist/quill.snow.css'
+import dynamic from "next/dynamic";
+
+
+const QuillNoSSRWrapper = dynamic(import('react-quill'),
+    {ssr: false,
+            loading: ()=> <p>Loading ...</p>})
+const adminArticle = () => {
+    const [formData, setData] = useState({
+        title: '',
+        description: '',
+        category: '',
+        content: '',
+    })
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('/api/createArticle', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await response.json();
+            console.log(data);
+            if (response.ok) {
+                // Store JWT token in local storage
+                // localStorage.setItem('token', data.token);
+
+
+                // Redirect to homepage
+                window.location.href = '/Dashboard/Profile';
+            }
+        } catch (error) {
+
+            console.error(error);
+        }
+    }
+    return(
+        <DashboardLayout>
+            <section className={" w-full grid gap-6"}>
+                <h1 className={"w-full text-xl font-bold text-gray-800 sm:text-2xl dark:text-white"}>Quel article voulez-vous poster ?</h1>
+                <div className={"grid place-content-stretch gap-4 w-full h-full rounded-xl p-0.5 bg-gradient-to-r from-transparent to-white"}>
+                    <div className={"grid rounded-xl p-6 h-full w-full bg-cardTransparent "}>
+                        <form onSubmit={handleFormSubmit}>
+                            <ul className={"gap-6 grid"}>
+                                <li>
+                                    <label htmlFor="subject"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre de l'article</label>
+                                    <input type="text" name="title" id="title"
+                                           className="bg-gray-50 w-full border border-grey text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           placeholder="" required={true}
+                                           value={title}
+                                           onChange={handleInputChange}/>
+                                </li>
+                                <li>
+                                    <label htmlFor="description"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                                    <input type="text" name="description" id="description"
+                                           className="bg-gray-50 w-full h-20 border border-grey text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           placeholder="" required={true}
+                                           value={description}
+                                           onChange={handleInputChange}
+                                    />
+                                </li>
+                                <li>
+                                    <label htmlFor="name"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                                    <select
+                                        name="category" id="category"
+                                        required={true}
+                                        value={category}
+                                        onChange={handleInputChange}
+                                        className="bg-gray-50 w-full border border-grey text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option selected>Choisissez une categorie</option>
+                                        <option>Nft</option>
+                                        <option>Immobilier</option>
+                                        <option>Start-up</option>
+                                        <option>Crypto-currency</option>
+                                        <option>Finances</option>
+                                    </select>
+                                </li>
+                                {/*Markdinw nécéssaire Rich text editor*/}
+                                <li>
+                                    <label htmlFor="content"
+
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contenu</label>
+
+                                    <QuillNoSSRWrapper
+                                        id={"content"}
+                                        value={content}
+                                        onChange={handleInputChange}
+                                        theme={"snow"}
+                                        className="bg-gray-50 w-full border border-grey text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                                </li>
+                                <li>
+                                    <div className=" items-center justify-center w-full">
+                                        <label htmlFor="detail"
+                                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image de couverture</label>
+                                        <label htmlFor="dropzone-file"
+                                               className="flex flex-col items-center justify-center w-full h-58 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                                <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none"
+                                                     stroke="currentColor" viewBox="0 0 24 24"
+                                                     xmlns="http://www.w3.org/2000/svg">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+                                                </svg>
+                                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                                                    className="font-semibold text-primary">Ajouter un fichier</span> ou faites le glisser ici</p>
+                                            </div>
+                                            <input id="dropzone-file" type="file" className="hidden"/>
+                                        </label>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div className={"grid place-content-center pt-6"}>
+                                <button type={"submit"} className="grid w-fit place-content-center px-5 py-3 my-3 mr-3 text-base  text-center text-white rounded-full bg-primary transition duration-300 hover:shadow-xl hover:shadow-primary/30  focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                                    Poster
+                                </button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </section>
+        </DashboardLayout>
+    )
+}
+
+export default adminArticle;
