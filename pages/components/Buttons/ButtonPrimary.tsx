db8e85192eb0b3a770fc65593c2dae9777e59f67
LaChancee <miharisoababef@gmail.com>
2023-05-13 15:29:57 +0200

    Création du composant bouton primaires

---
+++ pages/components/Buttons/ButtonPrimary.tsx
@@ -0,0 +1,9 @@
+const ButtonPrimary = ({text}: { text: string }) => {
+    return (
+        <button
+            className={"inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-full bg-primary transition duration-300 hover:shadow-xl hover:shadow-primary/30  focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"}>
+            {text}
+        </button>
+    )
+}
+export default ButtonPrimary
