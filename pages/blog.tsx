9bc3ef91327d4e23f20d354897733ac11d20f715
LaChancee <miharisoababef@gmail.com>
2023-05-30 22:37:37 +0200

    Changemenbt de lien

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -43,7 +43,7 @@ export default function Blog() {
 
 
     useEffect(() => {
-            axios.get("https://investitsyi-production.up.railway.app/api/article/getArticles")
+            axios.get("api/article/getArticles")
                 .then((response) => {
                     setArticles(response.data);
                 })
